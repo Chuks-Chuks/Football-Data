67e768c8c2470885768b762ec390e09e81f028bb
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-01-30 01:19:47 +0000

    I altered the iterator (i) for season number which looked like a much better name

--- fetch_data.py
+++ fetch_data.py
@@ -128,16 +128,16 @@ def return_clubs(clubs_list: list):
                 new_list.append(j)
     return new_list
 
-for i in range(0, len(season_dropdown.text.split())):
+for season_number in range(0, len(season_dropdown.text.split())):
     try:
 
-        print(f"\n\nWe are currently in the error free zone. We are trying the {i} path\n")
-        get_season(i)
+        print(f"\n\nWe are currently in the error free zone. We are trying the {season_number} path\n")
+        get_season(season_number)
         get_clubs_list()
         seasons.click()
         time.sleep(3)
     except e.ElementClickInterceptedException:
-        print(f"\n\n We are currently in the error zone. This is the {i} iteration\n")
+        print(f"\n\n We are currently in the error zone. This is the {season_number} iteration\n")
         scroll_dropdown(season_dropdown, year[-1])
         time.sleep(3)
     # except e.ElementNotInteractableException:

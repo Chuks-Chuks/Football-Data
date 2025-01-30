77db82059063bed1f589f12ad1607f97b2db8503
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-01-30 02:52:43 +0000

    Updating the changes in the main and database connect

--- database_connect.py
+++ database_connect.py
@@ -1,10 +1,13 @@
 import psycopg2 # importing this to enable me to connect to my PostgreSQL database
-connection = psycopg2.connect(
-    user='postgres',
-    password='Nkemdebe@2024',
-    host='localhost',
-    port='5432',
-    database='postgres'
-)
 
-cursor = connection.cursor()
+class WriteToDatabase:
+    def __init__(self):
+        self.connection = psycopg2.connect(
+                user='postgres',
+                password='Nkemdebe@2024',
+                host='localhost',
+                port='5432',
+                database='postgres'
+            )
+        self.cursor = self.connection.cursor()
+
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-for season_number in range(0, len(season_dropdown.text.split())):
+from fetch_data import FetchData
+import time
+
+fetch = FetchData()
+
+season_dropdown = fetch.season_text().split()
+
+for season_number in range(0, len(season_dropdown)):
     try:
         print(f"\n\nWe are currently in the error free zone. We are trying the {season_number} path\n")
-        get_season(season_number)
-        get_clubs_list()
-        seasons.click()
+        fetch.get_season(season_number)
+        fetch.get_clubs_list()
+        fetch.pinpoint_dropdown()[0].click()
         time.sleep(3)
-    except e.ElementClickInterceptedException:
+    except fetch.exceptions.ElementClickInterceptedException:
         print(f"\n\n We are currently in the error zone. This is the {season_number} iteration\n")
-        scroll_dropdown(season_dropdown, year[-1])
+        fetch.scroll_dropdown(season_dropdown, fetch.year[-1])
         time.sleep(3)
